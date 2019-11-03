bb6ceb8a13278be75dc0126a4be5be0fbf45b140
Maris Mols <maris9229@gmail.com>
2019-11-03 20:30:47 +0200

    Created Header and Footer

---
+++ src/components/Header/Header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './../../logo.svg'
+
+class Header extends React.Component {
+    render() {
+        return(
+            <header>
+                <div className="logo">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </div>
+                <div className="categories">
+                    <div className="categories__entity">Women</div>
+                    <div className="categories__entity">Men</div>
+                </div>
+                <div className="site-search">
+                    <input type="text" className="site-search__input" />
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
